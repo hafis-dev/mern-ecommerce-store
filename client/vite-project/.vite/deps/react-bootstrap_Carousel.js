1e09f45a35c1387dcbfce00b14b5dcee424f2649
Muhammed Hafis <muhammedhafis302@gmail.com>
2025-12-10 21:30:15 +0530

    .gitignore updated

---
+++ client/vite-project/.vite/deps/react-bootstrap_Carousel.js
@@ -0,0 +1,11 @@
+"use client";
+import {
+  Carousel_default
+} from "./chunk-NW72UHIQ.js";
+import "./chunk-R3OQ3HAY.js";
+import "./chunk-KP7BIHBJ.js";
+import "./chunk-ULSRCYB6.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  Carousel_default as default
+};
