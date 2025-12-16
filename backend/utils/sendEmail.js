eb6c27d0e51a7444a150b566a194f6c988ec9e4e
Muhammed Hafis <muhammedhafis302@gmail.com>
2025-12-16 23:22:40 +0530

    Email Otp updated

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,14 +2,11 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_EMAIL,
     pass: process.env.SMTP_PASSWORD,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
